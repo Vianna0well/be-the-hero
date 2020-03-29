07c7691e3b1976926a30292b16358e016aeb0ac8
vianna <vianna8523@gmail.com>
2020-03-29 03:24:37 -0300

    funcionalidades 1

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Routes = express.Router();
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const OngController = require('./controllers/OngController');
 const Incidents = require('./controllers/IncidentsController');
 const Profile = require('./controllers/ProfileController');
@@ -8,7 +10,15 @@ const Sessions = require('./controllers/SessionController')
 Routes.post('/sessions', Sessions.create)
 
 Routes.get('/ongs', OngController.index);
-Routes.post('/ongs', OngController.create);
+Routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}) , OngController.create);
 
 Routes.get('/profile', Profile.index);
 
